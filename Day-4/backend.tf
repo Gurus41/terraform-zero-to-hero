3a8572fdd2eafb477138d86219e151536998712e
Gurus41 <guruswamich@icloud.com>
2024-04-21 13:31:21 +0530

    upadte main.tf & backend.fc files

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "gurus41-s3-demos" # change this name
+    bucket         = "gurus41-s3-demos" # change this 
     key            = "gurus/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
--- Day-4/main.tf
+++ Day-4/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 resource "aws_instance" "gurus41" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this name
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this name
+  ami = "ami-053b0d53c279acc90" # change this 
+  subnet_id = "subnet-019ea91ed9b5252e7" # change this 
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "gurus41-s3-demoss" # change this name
+  bucket = "gurus41-s3-demoss" # change this 
    # To delete the S3 bucket command "aws s3 rb s3://gurus41-s3-demosse --force  "
 }
 
