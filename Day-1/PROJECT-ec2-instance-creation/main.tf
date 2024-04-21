9b0c1ac6ddda3dfbf908ccb6ca5d9d0692cb10f6
Gurus41 <guruswamich@icloud.com>
2024-04-21 13:45:19 +0530

    initial files for Day-1

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-080e1f13689e17408"  # Specify an appropriate AMI ID
+    ami           = "ami-080e1f13689e17408"  # Specify an appropriate ami id
     instance_type = "t2.micro"
     subnet_id = "subnet-026a718065ff86449"
     key_name = "Gurusai"
